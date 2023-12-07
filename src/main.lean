fc53a42d80727fe5a4a4ff0eac15b25573b36186
theorasparrow <152971598+theorasparrow@users.noreply.github.com>
2023-12-07 15:37:52 +0100

    Update main.lean

--- src/main.lean
+++ src/main.lean
@@ -1,14 +1,1 @@
-theorem test : 1 + 1 = 2 :=
-begin
-  refl
-end
-
-theorem test_2 : 2 + 2 = 4 :=
-begin
-  refl
-end
-
-theorem test_3 : 3 + 3 = 6 :=
-begin
-  refl
-end
+-- theorem test : 1 + 1 = 2 := sorry
